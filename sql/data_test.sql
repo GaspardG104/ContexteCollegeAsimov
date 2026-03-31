9f06f62390b776b77cfe6c1142015ab8aa4bb697
Guidetti Gaspard <gaspardguidetti@gmail.com>
2026-03-31 15:40:22 +0200

    Connexion à la bdd et données de tests

---
+++ sql/data_test.sql
@@ -0,0 +1,8 @@
+INSERT INTO parents (nom, prenom, email, tel_portable) 
+VALUES ('Vernes', 'Pierre', 'p.vernes@email.fr', '0601020304');
+
+INSERT INTO classes (niveau, lettre, annee_scolaire) 
+VALUES (6, 'A', '2025-2026');
+
+INSERT INTO eleves (nom, prenom, identifiant_import, id_parent, id_classe_actuelle) 
+VALUES ('Vernes', 'Jules', 'jvernes01', 1, 1);
