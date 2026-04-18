7d295aaf5dc81e09a849967f50ac8aa4dccbb44e
Guidetti Gaspard <gaspardguidetti@gmail.com>
2026-04-18 21:28:54 +0200

    nettoyage coms

--- routes/projetRoutes.js
+++ routes/projetRoutes.js
@@ -10,7 +10,6 @@ router.get('/view', projetController.viewAllProjets);
 // --- ROUTES POUR LA CRÉATION (CREATE) ---
 
 // Affiche le formulaire de création
-// Note : Tu devras créer renderCreateForm dans ton contrôleur
 router.get('/create', projetController.renderCreateForm);
 
 // Traite l'envoi du formulaire de création
@@ -23,8 +22,6 @@ router.get('/edit/:id', projetController.renderEditForm);
 
 // Traite l'envoi du formulaire de modification
 router.post('/edit/:id', projetController.updateProjet);
-
-// --- ROUTES POUR LA SUPPRESSION (DELETE) ---
 
 // Supprime un projet spécifique
 router.get('/delete/:id', projetController.deleteProjet);
